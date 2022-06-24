40be36ee42fe1eb5cbfe7cc3fbc590b83ff9e53b
LiYuForever <1107852688qq.com>
2022-06-24 11:18:53 +0800

    First addit

---
+++ src/main/java/com/atguigu/pojo/HellloWorld.java
@@ -0,0 +1,12 @@
+package com.atguigu.pojo;
+
+/**
+ * @author Pen Li
+ * @create 2022-06-24 11:16
+ */
+public class HellloWorld {
+    public static void main(String[] args) {
+        System.out.println("快乐Java");
+
+    }
+}
